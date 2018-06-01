e012df57e6de85800e43d05b3c0b2e5ab04e119d
calcf <hchenpeng@163.com>
2018-06-01 17:17:04 +0800

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,40 @@
+import os
+from app import create_app, db
+from app.models import User, Role, Post
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+from config import DevelopmentConfig
+from sqlalchemy import create_engine
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+engine = create_engine(DevelopmentConfig.SQLALCHEMY_DATABASE_URI)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post, engine=engine)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+
+    # 把数据库迁移到最新修订版本
+    upgrade()
+
+    # 创建用户角色
+    Role.insert_roles()
+
+    # 让所有用户都关注此用户
+    User.add_self_follows()
+    print('deploy over')
+
+if __name__ == '__main__':
+    manager.run()
